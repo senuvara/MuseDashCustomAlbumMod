b486b4cce0cc42c92c9c511c330a4e6931b3911f
某10 <0w0@0w0.pw>
2020-04-08 02:38:02 +0800

    Reborn

---
+++ MuseDashModManager/CustomInfo.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace MuseDashCustomAlbumMod
+{
+    public class CustomInfo
+    {
+        public static JToken UID()
+        {
+            return "custom";
+        }
+
+        public static JToken Title()
+        {
+            return "自定义谱面";
+        }
+
+        public static JToken PrefabsName()
+        {
+            return "AlbumCustom";
+        }
+
+        public static JToken Price()
+        {
+            return "Free";
+        }
+
+        public static JToken JsonName()
+        {
+            return "custom";
+        }
+
+        public static JToken NeedPurchase()
+        {
+            return "false";
+        }
+    }
+}
---
+++ MuseDashModManager/CustomMap.cs
@@ -0,0 +1,35 @@
+﻿using Assets.Scripts.GameCore;
+using Assets.Scripts.GameCore.Managers;
+using Assets.Scripts.PeroTools.Commons;
+using Assets.Scripts.PeroTools.Nice.Datas;
+using Assets.Scripts.PeroTools.Nice.Interface;
+using UnityEngine;
+
+namespace MuseDashCustomAlbumMod
+{
+    public class CustomMap
+    {
+        public string Name;
+        public string Artist;
+        public string Music;
+        public string MusicDemo;
+        public string BPM;
+        public string Scene;
+        public string UID;
+        public AudioClip MusicClip = null;
+        public AudioClip MusicDemoClip = null;
+        public Sprite CoverSprite = null;
+        public string[] Difficulty = new string[3];
+        public StageInfo[] stages = new StageInfo[] { null, null, null };
+        public string Cover
+        {
+            get => UID + "_cover";
+        }
+
+        public string Map
+        {
+            get => UID + "_map";
+        }
+
+    }
+}
